b16c8f91501fbcac8374dcf3541cdf9c5a2f8eb5
Jon.Hall.7 <Jon.Hall.7@c431f278-95dd-11de-9a8c-5f207592b8ba>
2009-10-30 16:07:19 +0000

    again

--- NoteShare/htdocs/controllers/CourseHomePage.Controller.php
+++ NoteShare/htdocs/controllers/CourseHomePage.Controller.php
@@ -4,8 +4,7 @@ include $_SERVER['DOCUMENT_ROOT'] . 'model/DataAbstractionLayer.php';
 
 function getSessionMetadata( $sessionid )
 {
-	return "
-	<?xml version=\"1.0\"?>
+	return "<?xml version=\"1.0\"?>
 	
 	<sessionMetadata id=\"11111\" 
 		course=\"Software Engineering Project\"
@@ -22,8 +21,7 @@ function getSessionMembers( $userid, $sessionid )
 // There'll probably be some changes here as we figure 
 // out how to show friend thumbnail photos etc.
 
-	return "
-	<?xml version=\"1.0\"?>	
+	return "<?xml version=\"1.0\"?>	
 	<memberList>
 	<member id=\"11111\" 
 		friend=\"True\">
