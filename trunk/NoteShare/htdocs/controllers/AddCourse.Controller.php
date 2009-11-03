014b94d1ff01c7927190bb916c04c7a441385ec0
Jon.Hall.7 <Jon.Hall.7@c431f278-95dd-11de-9a8c-5f207592b8ba>
2009-11-03 17:21:33 +0000

    Uh...editing on the server...won't happen again...

--- trunk/NoteShare/htdocs/controllers/AddCourse.Controller.php
+++ trunk/NoteShare/htdocs/controllers/AddCourse.Controller.php
@@ -68,24 +68,111 @@ function getSessionsXML( $courseName )
 
 ?>
 
-<script>
-<!--
-  function addControllers()
+<!----- Controller portion ------->
+<script type="text/javascript">
+  function getDepartments()
   {
-    var obj = document.getElementById( 'university' );
-    obj.addEventListener( 'onchange', function(e){
-      eventUniversity();
-      e.stopPropagation();
-      e.preventDefault();
-      return false;
-    }, false);
+    var cmbUni = document.getElementById( 'university' );
+    var selUni = cmbUni.selectedIndex;
+    var universityID = cmbUni.options[ selUni ].value;
+    //document.getElementById( 'testBox' ).innerHTML = universityID;
+
+    var xmlhttp;
+    if( window.XMLHttpRequest)
+    {
+      xmlhttp = new XMLHttpRequest();
+    }
+    else if ( window.ActiveXObject )
+    {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else
+    {
+      alert( "XMLHTTP!?" );
+    }
+
+    if( xmlhttp == null )
+      document.getElementById( 'testBox' ).innerHTML = "NULL!";
+
+    xmlhttp.onreadystatechange=function()
+    {
+      if( xmlhttp.readyState == 4 )
+      {
+        document.getElementById( 'department' ).innerHTML = xmlhttp.responseTex$
+      }
+    }
+    xmlhttp.open( "GET", "./view/ajaxView.php?function_name=getDepartments&univ$
+    xmlhttp.send( null );
   }
 
-  function eventUniversity()
+  function getCourses()
   {
-    document.getElementById( 'debugOutput' ).setTextValue( "active" );
+    var cmbDept = document.getElementById( 'department' );
+    var selDept = cmbDept.selectedIndex;
+    var deptID = cmbDept.options[ selDept ].value;
+    //document.getElementById( 'testBox' ).innerHTML = universityID;
+
+    var xmlhttp;
+    if( window.XMLHttpRequest)
+    {
+      xmlhttp = new XMLHttpRequest();
+    }
+    else if ( window.ActiveXObject )
+    {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else
+    {
+      alert( "XMLHTTP!?" );
+    }
+
+    if( xmlhttp == null )
+      document.getElementById( 'testBox' ).innerHTML = "NULL!";
+
+    xmlhttp.onreadystatechange=function()
+    {
+      if( xmlhttp.readyState == 4 )
+      {
+        document.getElementById( 'course' ).innerHTML = xmlhttp.responseText;
+      }
+    }
+    xmlhttp.open( "GET", "./view/ajaxView.php?function_name=getCourses&departme$
+    xmlhttp.send( null );
   }
-//-->
+
+  function getSessions()
+  {
+    var cmbCourse = document.getElementById( 'course' );
+    var selCourse = cmbCourse.selectedIndex;
+    var sessionID = cmbCourse.options[ selCourse ].value;
+    //document.getElementById( 'testBox' ).innerHTML = universityID;
+
+    var xmlhttp;
+    if( window.XMLHttpRequest)
+    {
+      xmlhttp = new XMLHttpRequest();
+    }
+    else if ( window.ActiveXObject )
+    {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else
+    {
+      alert( "XMLHTTP!?" );
+    }
+
+    if( xmlhttp == null )
+      document.getElementById( 'testBox' ).innerHTML = "NULL!";
+
+    xmlhttp.onreadystatechange=function()
+    {
+      if( xmlhttp.readyState == 4 )
+      {
+        document.getElementById( 'session' ).innerHTML = xmlhttp.responseText;
+      }
+    }
+    xmlhttp.open( "GET", "./view/ajaxView.php?function_name=getSessions&courseI$
+    xmlhttp.send( null );
+  }
+
 </script>
-
-
