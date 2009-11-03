5972a787f477c832042d438a22bdf961a28d4602
Jon.Hall.7 <Jon.Hall.7@c431f278-95dd-11de-9a8c-5f207592b8ba>
2009-11-03 03:42:17 +0000

    drop course 3

---
+++ NoteShare/htdocs/view/DropCourse.php
@@ -0,0 +1,19 @@
+<?php
+	require_once $_SERVER['DOCUMENT_ROOT'] . '../php/facebook.php';
+	require_once $_SERVER['DOCUMENT_ROOT'] . 'controllers/Session.Controller.php';
+	
+	include $_SERVER['DOCUMENT_ROOT'] . 'controllers/DropCourse.Controller.php';
+	
+	$session_id = $_POST['session'];
+	
+	ob_start();
+	removeUserSession($user_id, $session_id);
+	ob_end_clean();
+	
+	$host  = $_SERVER['HTTP_HOST'];
+	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$extra = "view/$_GET['from']";
+	
+	header("Location:http://$host$uri/$extra");
+	exit();
+?>
