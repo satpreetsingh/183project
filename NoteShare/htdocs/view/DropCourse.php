5051c347c5223de6ea91ffbbbcfbc3cb8d34d9c2
Jon.Hall.7 <Jon.Hall.7@c431f278-95dd-11de-9a8c-5f207592b8ba>
2009-11-03 03:43:54 +0000

    drop course 4

--- NoteShare/htdocs/view/DropCourse.php
+++ NoteShare/htdocs/view/DropCourse.php
@@ -12,7 +12,7 @@
 	
 	$host  = $_SERVER['HTTP_HOST'];
 	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-	$extra = "view/$_GET['from']";
+	$extra = "view/" . $_GET['from'];
 	
 	header("Location:http://$host$uri/$extra");
 	exit();
